ec1e2b8820a52812e8e8f91ca7694800fb6520e6
Abe Lourenco <alourenco@vistaprint.com>
2018-05-09 13:34:18 +0200

    Create_rock_question_with_int

---
+++ TriviaGame.Tests/GameShould.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using UglyTrivia;
+
+namespace TriviaGame.Tests
+{
+    [TestFixture]
+    public class GameShould
+    {
+        private Game game;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.game = new Game();
+        }
+
+        [Test]
+        public void Create_rock_question_with_int()
+        {
+            const int QUESTION_NUM = 1;
+
+            string question = this.game.createRockQuestion(QUESTION_NUM);
+
+            Assert.AreEqual(
+                "Rock Question " + QUESTION_NUM.ToString(),
+                question);
+        }
+    }
+}
